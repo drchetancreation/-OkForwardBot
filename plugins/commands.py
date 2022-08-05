d1499dca7e3c9dd60715f682b6849f4eba5911ca
Drchetancreation <107134390+drchetancreation@users.noreply.github.com>
2022-08-05 16:57:30 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -19,7 +19,7 @@ async def start(client, message):
         InlineKeyboardButton('Update Channel ♻️', url='https://t.me/drchetancreations')
         ],[
         InlineKeyboardButton('💡 SouceCode', url='https://github.com/drchetancreation'),
-        InlineKeyboardButton('String Session 🎻', url ='https://telegram.com/SessionStringGeneratorCBot')
+        InlineKeyboardButton('🆔BOT', url ='https://telegram.com/Idsdbot')
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
     await client.send_message(
