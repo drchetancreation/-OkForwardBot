172b55fa3f4ab7e55fc1d96d14d045f58d507832
Drchetancreation <107134390+drchetancreation@users.noreply.github.com>
2022-08-05 11:57:44 +0530

    Update regix.py

--- plugins/regix.py
+++ plugins/regix.py
@@ -84,9 +84,9 @@ async def pub_(bot, message):
                 await m.edit_text(f'Error: {e}')
             else:
                 buttons = [[
-                    InlineKeyboardButton('📜 Support Group', url='https://t.me/DxHelpDesk')
+                    InlineKeyboardButton('📜 Support Group', url='https://t.me/drchetancreations')
                     ],[
-                    InlineKeyboardButton('📡 Update Channel', url='https://t.me/DX_Botz')
+                    InlineKeyboardButton('📡 Update Channel', url='https://t.me/drchetancreations')
                 ]]
                 reply_markup = InlineKeyboardMarkup(buttons)
                 await m.edit_text(
